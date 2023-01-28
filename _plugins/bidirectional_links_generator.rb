c3e12e277a4e21bd36395b243d8563d2009ca4b5
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-01-28 15:43:33 +0900

    hotfix: 베포시에 그래프가 보이지 않는 문제 해결

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -72,12 +72,38 @@ class BidirectionalLinksGenerator < Jekyll::Generator
         HTML
       )
     end
+
+     # Identify note backlinks and add them to each note
+     all_notes.each do |current_note|
+      # Nodes: Jekyll
+      notes_linking_to_current_note = all_notes.filter do |e|
+        e.content.include?(current_note.url)
+      end
+
+      # Nodes: Graph
+      graph_nodes << {
+        id: note_id_from_note(current_note),
+        path: "#{site.baseurl}#{current_note.url}#{link_extension}",
+        label: current_note.data['title'],
+      } unless current_note.path.include?('_notes/index.html')
+
+      # Edges: Jekyll
+      current_note.data['backlinks'] = notes_linking_to_current_note
+
+      # Edges: Graph
+      notes_linking_to_current_note.each do |n|
+        graph_edges << {
+          source: note_id_from_note(n),
+          target: note_id_from_note(current_note),
+        }
+      end
+    end
+
     File.write('_includes/notes_graph.json', JSON.dump({
       edges: graph_edges,
       nodes: graph_nodes,
     }))
-    end
-
+  end
   
 
   def note_id_from_note(note)
